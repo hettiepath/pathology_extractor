c9d76123eeb9a61fb7bc953d478955000a88c231
titipata <titipat.a@u.northwestern.edu>
2016-04-19 20:38:12 -0500

    Update read report function

--- extractors/README.md
+++ extractors/README.md
@@ -3,5 +3,6 @@
 This folder contains script to read and preprocess pathology reports and
 progress notes.
 
-**Parse progress notes** we can format plain input text to structure `csv` by
-running `python report2csv.py -i progress_notes.txt -o progress_notes_out.csv`
+**Parse progress notes** we can format plain input text to structure `csv` file by
+running `python report2csv.py -i input.txt -o output.csv`. You can run
+`python report2csv.py --help` in order to see the instruction.
--- extractors/report2csv.py
+++ extractors/report2csv.py
@@ -5,22 +5,13 @@ import argparse
 import pandas as pd
 from unidecode import unidecode
 from nltk.tokenize import WhitespaceTokenizer
-
-
-def is_valid_file(parser, arg):
-    """
-    ref: http://stackoverflow.com/questions/11540854/
-    """
-    if not os.path.exists(arg):
-        parser.error("The file %s does not exist!" % arg)
-    else:
-        return open(arg, 'r')
+w_tokenizer = WhitespaceTokenizer()
 
 
 def read_notes(file_name):
     """Read pathology report lines"""
     lines = [l for l in csv.reader(open(file_name, 'r'))][1::]
-    lines_join = list(map(lambda text: unidecode(' '.join(text)), lines))
+    lines_join = list(map(lambda text: ' '.join(text), lines))
     return lines_join
 
 
@@ -28,30 +19,32 @@ def process_notes(line):
     """
     Split given string from text file with '|' separator
     format of given text is as follows
-        id||date||||pathology text
+        id|||||date||||||||pathology text
     """
     splits = re.split('\|\|\|\|\|', line)
     idx = int(re.sub('\t', '', splits[0])) # identification number
     date = re.sub('\|', '', splits[1])
     contexts = ' '.join(splits[2::])
+    contexts = unidecode(contexts)
     contexts_tokenize = w_tokenizer.tokenize(contexts)
     return [idx, date, ' '.join(contexts_tokenize)]
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Progress note text parser")
+    parser = argparse.ArgumentParser(description="Progress notes/ Pathology text parser")
     parser.add_argument('-i', '--input', dest='input', required=True,
-                        help="input pathology file",
-                        type=lambda x: is_valid_file(parser, x))
+                        help="input text file of pathology or progress notes")
     parser.add_argument('-o', '--output', dest='output',
-                        default='progress_notes_out.csv')
+                        default='progress_notes_out.csv',
+                        help="output file name for structured csv progress notes")
 
     args = parser.parse_args()
 
-    print('read and process progress notes from %s' % args.input)
+    print('read and process progress notes from %s ...' % args.input)
     progress_notes = read_notes(args.input)
-    progress_notes_proc = list(map(process_notes, progress_notes))
+    progress_notes_proc = list(map(lambda x : process_notes(x), progress_notes))
+    print('done!...')
     progress_notes_proc_df = pd.DataFrame(progress_notes_proc,
                                           columns=['identification', 'date', 'report'])
-    print('process text documents and saved to %s' % args.output)
+    print('process text documents and saved to %s ...' % args.output)
     progress_notes_proc_df.to_csv(args.output, index=False)
