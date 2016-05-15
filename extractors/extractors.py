e0bbe481290ee71043c38b4c0cedf9c064d28809
titipata <titipat.a@u.northwestern.edu>
2016-05-15 04:20:55 -0500

    Group tag function from NER output

--- extractors/extractors.py
+++ extractors/extractors.py
@@ -1,3 +1,5 @@
+from itertools import groupby
+from operator import itemgetter
 import re
 from unidecode import unidecode
 from nltk.tokenize import sent_tokenize
@@ -11,17 +13,48 @@ st = StanfordNERTagger(taggers[0])
 
 
 def split(s):
+    """
+    Simple split sentence
+    """
     s_split = sent_tokenize(unidecode(s), language='english')
     return s_split
 
 def tag(s):
     """
-    Tokenize and apply name entity recognition to tag input string
+    Tokenize and apply name entity recognition (NER) to tag input string
+
+    Parameters
+    ----------
+    s: str, input string
+
+    Returns
+    -------
+    ner_tag: list of tuple, NER tag output
     """
     s_tokenize = word_tokenize(s)
-    return st.tag(s_tokenize)
+    ner_tag = st.tag(s_tokenize)
+    return ner_tag
 
 def group_tag(s):
-    ner_tag = st.tag(s)
-    # group same tag together
-    return None
+    """
+    Group consecutive out tuple from NER with key (second element of tuple)
+
+    Parameters
+    ----------
+    s: str, input string
+
+    Returns
+    -------
+    ner_tag_group: list of tuple, group NER tag output
+
+    Example
+    -------
+    >> group_tag('Rami Eid is studying at Stony Brook University in NY')
+    """
+    ner_tag = tag(s)
+    # group by consecutive key
+    ner_tag_group = list()
+    for key, group in groupby(ner_tag, itemgetter(1)):
+        entity = ' '.join([g[0] for g in group])
+        ner_tag_group.append(tuple((entity, key)))
+    return ner_tag_group
